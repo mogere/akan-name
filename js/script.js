a09e28e86fa446b0ccffa37864a4b3dba115f7e0
mogere <pmnyatichi@gmail.com>
2019-10-25 16:16:13 +0300

    fix bug for capturing radio data

--- js/script.js
+++ js/script.js
@@ -13,9 +13,10 @@ function display(){
     var checkedValue = null;
     gender = document.getElementsByName("gender");
         for(var i=0; gender[i]; ++i){
-            if(gender[i].checked)
+            if(gender[i].checked){
                  checkedValue = gender[i].value;
             break;
+            }
         }
     alert(checkedValue);
     var boysNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"];
