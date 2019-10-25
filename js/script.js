3c5a42efb9fbbe767f678b6b38a6ca4f2e194763
mogere <pmnyatichi@gmail.com>
2019-10-25 11:47:54 +0300

    add display function

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
+
+var dayIndex;
 function calculateDay(){
     var date = document.getElementById("birthdate").value;
-    var cc = date.slice(0,2);
-    var yy = date.slice(2,4);
-    var mm = date.slice(5,7);
-    var dd = date.slice(8);
-    alert(cc + "-" + yy + "-" + mm + "-" +  dd);
+    // var cc = date.slice(0,2);
+    // var yy = date.slice(2,4);
+    // var mm = date.slice(5,7);
+    // var dd = date.slice(8);
+    // alert(cc + "-" + yy + "-" + mm + "-" +  dd);
    
-    var day_of_week  = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7
+    // var day_of_week  = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7
 
-    alert(day_of_week);
+    //alert(day_of_week);
+
+    var dt = new Date(date);
+    dayIndex = dt.getDay();
+    alert(dayIndex);
+    display();
+    
+}
+
+function display(){
+    if (dayIndex === 0)
+    alert("Sunday");
 
 }
