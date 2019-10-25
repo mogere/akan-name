2dda41942d49640570ef8ddf2e4f5c6b872eae9e
mogere <pmnyatichi@gmail.com>
2019-10-25 11:18:33 +0300

    add initial function logic

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 function calculateDay(){
-    var date = document.getElementById("birthdate");
-    alert(date);
+    var date = document.getElementById("birthdate").value;
+    var cc = date.slice(0,2);
+    var yy = date.slice(2,4);
+    var mm = date.slice(5,7);
+    var dd = date.slice(8);
+    alert(cc + "-" + yy + "-" + mm + "-" +  dd);
+   
+    var day_of_week  = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7
+
+    alert(day_of_week);
+
 }
