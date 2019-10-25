0934413639ec5932aefd9d854eda7d9bb2fdda22
mogere <pmnyatichi@gmail.com>
2019-10-25 17:07:11 +0300

    add conditional logic

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 var dayIndex, gender;
+var boysNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"];
+var girlsNames = ["Akosua", "Adwoa", "Abenaa", "Akua","Yaa", "Afua", "Ama"];
 
 function calculateDay(){
     var date = document.getElementById("birthdate").value;
     var dt = new Date(date);
     dayIndex = dt.getDay();
-    alert(dayIndex);
-    display();
-    
+    display(); 
 }
 
 function display(){
@@ -18,12 +18,65 @@ function display(){
             break;
             }
         }
-    alert(checkedValue);
-    var boysNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"];
-    var girlsNames = ["Akosua", "Adwoa", "Abenaa", "Akua","Yaa", "Afua", "Ama"];
-    if (dayIndex === 0 && checkedValue === male)
-    {}
-
-     //var lName = document.getElementById("name").value = ""+"";
+   
+        
+    if (dayIndex === 0 && checkedValue === "male")
+    {
+        var akan = boysNames[0];
+        alert(akan);
+        document.getElementById("akan").value = lname + boysNames[0];
+    }
+    else if (dayIndex === 1 && checkedValue === "male"){
+        var akan = boysNames[1];
+        alert(akan);
+    }
+    else if (dayIndex === 2 && checkedValue === "male"){
+        var akan = boysNames[2];
+        alert(akan);  
+    }
+    else if (dayIndex === 3 && checkedValue === "male"){
+        var akan = boysNames[3];
+        alert(akan);
+    }
+    else if (dayIndex === 4 && checkedValue === "male"){
+        var akan = boysNames[4];
+        alert(akan);
+    }
+    else if (dayIndex === 5 && checkedValue === "male"){
+        var akan = boysNames[5];
+        alert(akan);
+    }
+    else if (dayIndex === 6 && checkedValue === "male"){
+        var akan = boysNames[6];
+        alert(akan);
+    }
+    else if (dayIndex === 0 && checkedValue === "female"){
+        var akan = girlsNames[0];
+        alert(akan);
+    }
+    else if (dayIndex === 1 && checkedValue === "female"){
+        var akan = girlsNames[1];
+        alert(akan);  
+    }
+    else if (dayIndex === 2 && checkedValue === "female"){
+        var akan = girlsNames[2];
+        alert(akan);
+    }
+    else if (dayIndex === 3 && checkedValue === "female"){
+        var akan = boysNames[3];
+        alert(akan);
+    }
+    else if (dayIndex === 4 && checkedValue === "female"){
+        var akan = girlsNames[4];
+        alert(akan);
+    }
+    else if (dayIndex === 5 && checkedValue === "female"){
+        var akan = girlsNames[5];
+        alert(akan);
+    }
+    else if (dayIndex === 6 && checkedValue === "female"){
+        var akan = girlsNames[6];
+        alert(akan);
+    }
 
 }
